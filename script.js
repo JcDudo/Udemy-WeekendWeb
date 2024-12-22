ccdf38e9fe5bed1e9e44c284a37c76f93a18e785
Allen Yang <118941900+JcDudo@users.noreply.github.com>
2024-12-21 18:12:41 -0800

    DOM

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("Hello World!");
 
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+console.log(btn);
+console.dir(btn);
 
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
@@ -15,25 +19,25 @@ btn.addEventListener("click", function () {
 
 // console.log(parseInt("24.533ccc"));
 
-function calcFactAge(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
+// function calcFactAge(year) {
+//   const currentYear = new Date().getFullYear();
+//   const age = currentYear - year;
 
-  if (age >= 0) return age;
-  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
-}
+//   if (age >= 0) return age;
+//   else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+// }
 
-const age1 = calcFactAge(2015);
-console.log(age1);
-console.log(calcFactAge(2055));
+// const age1 = calcFactAge(2015);
+// console.log(age1);
+// console.log(calcFactAge(2055));
 
-const calcFactAge2 = (year) =>
-  year <= new Date().getFullYear()
-    ? new Date().getFullYear() - year
-    : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
+// const calcFactAge2 = (year) =>
+//   year <= new Date().getFullYear()
+//     ? new Date().getFullYear() - year
+//     : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
 
-console.log(calcFactAge2(2016));
-console.log(calcFactAge2(2027));
+// console.log(calcFactAge2(2016));
+// console.log(calcFactAge2(2027));
 
 // let votesInteresting = 19;
 // let votesMindBlowing = 20;
@@ -67,97 +71,97 @@ console.log(calcFactAge2(2027));
 // }`;
 // console.log(str);
 
-const fact = ["Lisbon is the capital of Portual", 2015, true];
+// const fact = ["Lisbon is the capital of Portual", 2015, true];
 
-console.log(fact);
-console.log(fact[2]);
-console.log(fact.length);
+// console.log(fact);
+// console.log(fact[2]);
+// console.log(fact.length);
 
-const [text, createdIn] = fact;
-console.log(createdIn);
+// const [text, createdIn] = fact;
+// console.log(createdIn);
 
-// Copy array to a new array
-const newFact = [...fact, "society"];
-console.log(newFact);
+// // Copy array to a new array
+// const newFact = [...fact, "society"];
+// console.log(newFact);
 
-[2, 4, 6, 8].forEach(function (el) {
-  console.log(el);
-});
-
-// [2, 4, 6, 8].map(function (el) {
-//     return el * 10;
+// [2, 4, 6, 8].forEach(function (el) {
+//   console.log(el);
 // });
 
-const times10 = [2, 4, 6, 8].map((el) => el * 10);
-console.log(times10);
+// // [2, 4, 6, 8].map(function (el) {
+// //     return el * 10;
+// // });
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
+// const times10 = [2, 4, 6, 8].map((el) => el * 10);
+// console.log(times10);
 
-const allCategories = CATEGORIES.map((el) => el.name);
-console.log(allCategories);
+// const CATEGORIES = [
+//   { name: "technology", color: "#3b82f6" },
+//   { name: "science", color: "#16a34a" },
+//   { name: "finance", color: "#ef4444" },
+//   { name: "society", color: "#eab308" },
+//   { name: "entertainment", color: "#db2777" },
+//   { name: "health", color: "#14b8a6" },
+//   { name: "history", color: "#f97316" },
+//   { name: "news", color: "#8b5cf6" },
+// ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+// const allCategories = CATEGORIES.map((el) => el.name);
+// console.log(allCategories);
 
-const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
-console.log(factAges);
-console.log(factAges.join("-"));
+// const initialFacts = [
+//   {
+//     id: 1,
+//     text: "React is being developed by Meta (formerly facebook)",
+//     source: "https://opensource.fb.com/",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+//     source:
+//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+//     category: "society",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "Lisbon is the capital of Portugal",
+//     source: "https://en.wikipedia.org/wiki/Lisbon",
+//     category: "society",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
 
-const factObj = {
-  text: "Lisbon is the capital of Portual",
-  category: "society",
-  createdIn: 2015,
-  isCorrect: true,
-  createSummary: function () {
-    return `The fact ${
-      this.text
-    } is from the category ${this.category.toUpperCase()}`;
-  },
-};
+// const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
+// console.log(factAges);
+// console.log(factAges.join("-"));
 
-console.log(factObj.text);
-console.log(factObj["text"]);
+// const factObj = {
+//   text: "Lisbon is the capital of Portual",
+//   category: "society",
+//   createdIn: 2015,
+//   isCorrect: true,
+//   createSummary: function () {
+//     return `The fact ${
+//       this.text
+//     } is from the category ${this.category.toUpperCase()}`;
+//   },
+// };
 
-const { category, isCorrect } = factObj;
-console.log(category);
-console.log(factObj.createSummary());
+// console.log(factObj.text);
+// console.log(factObj["text"]);
+
+// const { category, isCorrect } = factObj;
+// console.log(category);
+// console.log(factObj.createSummary());

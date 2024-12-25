cb225ed8d1fa77329daebb3b72838e66b1ff94ea
Allen Yang <118941900+JcDudo@users.noreply.github.com>
2024-12-24 23:30:57 -0800

    Create DOM element

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 console.log("Hello World!");
 
+//selecting DOM element
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 const factsList = document.querySelector(".facts-list");
 
-console.log(btn);
-console.dir(btn);
+//Create DOM elements: Render fact in list
+factsList.innerHTML = "";
 
-btn.addEventListener("click", function () {
-  if (form.classList.contains("hidden")) {
-    form.classList.remove("hidden");
-    btn.textContent = "Close";
-  } else {
-    form.classList.add("hidden");
-    btn.textContent = "Share a fact";
-  }
-});
+// factsList.insertAdjacentHTML("afterbegin", "<li>Jonas</li>");
+// factsList.insertAdjacentHTML("afterbegin", "<li>Mike</li>");
+createFactsList(initialFacts);
 
+function createFactsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+        ${fact.text}
+            <a
+            class="source"
+            href="${fact.source}"
+            target="_blank"
+            >(Source)</a>
+        </p>
+        <span class="tag" style="background-color: #3b82f6"
+        >${fact.category}</span
+        ><li>`
+  );
+
+  console.log(htmlArr);
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+
+  console.log(btn);
+  console.dir(btn);
+
+  btn.addEventListener("click", function () {
+    if (form.classList.contains("hidden")) {
+      form.classList.remove("hidden");
+      btn.textContent = "Close";
+    } else {
+      form.classList.add("hidden");
+      btn.textContent = "Share a fact";
+    }
+  });
+}
 // console.log(parseInt("24.533ccc"));
 
 // function calcFactAge(year) {
@@ -80,7 +140,7 @@ btn.addEventListener("click", function () {
 // const [text, createdIn] = fact;
 // console.log(createdIn);
 
-// // Copy array to a new array
+// Copy array to a new array
 // const newFact = [...fact, "society"];
 // console.log(newFact);
 
@@ -88,9 +148,9 @@ btn.addEventListener("click", function () {
 //   console.log(el);
 // });
 
-// // [2, 4, 6, 8].map(function (el) {
-// //     return el * 10;
-// // });
+// [2, 4, 6, 8].map(function (el) {
+//     return el * 10;
+// });
 
 // const times10 = [2, 4, 6, 8].map((el) => el * 10);
 // console.log(times10);
@@ -108,40 +168,6 @@ btn.addEventListener("click", function () {
 
 // const allCategories = CATEGORIES.map((el) => el.name);
 // console.log(allCategories);
-
-// const initialFacts = [
-//   {
-//     id: 1,
-//     text: "React is being developed by Meta (formerly facebook)",
-//     source: "https://opensource.fb.com/",
-//     category: "technology",
-//     votesInteresting: 24,
-//     votesMindblowing: 9,
-//     votesFalse: 4,
-//     createdIn: 2021,
-//   },
-//   {
-//     id: 2,
-//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-//     source:
-//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-//     category: "society",
-//     votesInteresting: 11,
-//     votesMindblowing: 2,
-//     votesFalse: 0,
-//     createdIn: 2019,
-//   },
-//   {
-//     id: 3,
-//     text: "Lisbon is the capital of Portugal",
-//     source: "https://en.wikipedia.org/wiki/Lisbon",
-//     category: "society",
-//     votesInteresting: 8,
-//     votesMindblowing: 3,
-//     votesFalse: 1,
-//     createdIn: 2015,
-//   },
-// ];
 
 // const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
 // console.log(factAges);

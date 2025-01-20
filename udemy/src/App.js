92793f22aa3a574bea184a5466395b51098eda60
Allen Yang <118941900+JcDudo@users.noreply.github.com>
2025-01-19 22:11:26 -0800

    Final build

--- udemy/src/App.js
+++ udemy/src/App.js
@@ -278,7 +278,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
-
+  const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
   async function handleVote(columnName) {
     setIsUpdating(true);
     const {data: updatedFact, error} = await supabase
@@ -288,7 +288,7 @@ function Fact({ fact, setFacts }) {
       .select();
     setIsUpdating(false); 
     
-    console.log(updatedFact);
+    
     if (!error)
       setFacts((facts) =>
       facts.map((f) => f.id === fact.id ?
@@ -298,6 +298,8 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">
+        [🚫DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">
           (Source)
